4f2ed680dfa907e075c27d1c9a0587c9a17f7b43
LuisHGH <luishenriquegh2701@gmail.com>
2021-01-15 14:37:29 -0300

    Added bin folder :sparkles:

---
+++ bin/keyboard_layout_switch.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+# switch between my layouts
+
+# If an explicit layout is provided as an argument, use it. Otherwise, select the next layout from
+# the set [us, it, fr].
+if [[ -n "$1" ]]; then
+    setxkbmap "$1"
+else
+    layout=$(setxkbmap -query | awk 'END{print $2}')
+    case $layout in
+        us)
+                setxkbmap br
+            ;;
+        br)
+                setxkbmap us
+            ;;
+        *)
+                setxkbmap us
+            ;;
+    esac
+fi
